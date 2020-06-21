75d58d84a9fd3c738230702952b90eba2f929e94
sachinkolhe <sachukolhe@gmail.com>
2020-06-21 17:43:43 +0530

    java test 1b

--- JavaTest/src/com/synechron/Test1b.java
+++ JavaTest/src/com/synechron/Test1b.java
@@ -8,17 +8,17 @@ public class Test1b {
 		 * A non-final local variable or method parameter whose value is never
 		 * changed after initialization is known as effectively final
 		 */
-		String effectivelyFinal = "Hello ";
+		String eFinal = "Hello ";
 		Printer printer = new Printer() {
 			
 			@Override
 			public void print(String text) {
-				String print = effectivelyFinal + text;
+				String print = eFinal + text;
 				System.out.println(print);
-				//effectivelyFinal += "Hi";
+				//eFinal += "Hi";
 				/**
-				 * If we try to change the value of variable it throws an error showing
-				 * Local variable effectivelyFinal defined in an enclosing scope must be final or effectively final
+				 * If we try to change the value of variable it throws an compilation error showing
+				 * Local variable eFinal defined in an enclosing scope must be final or effectively final
 				 * */
 			}
 		};
